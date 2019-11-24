e0c2d7237127a9decbc5ec49573f2aa53b06949a
angelkyh <angelkyh@chol.com>
2019-11-24 10:18:20 +0900

    소스트리 마지막 테스트

--- src/main/java/com/example/demo/Example01.java
+++ src/main/java/com/example/demo/Example01.java
@@ -9,5 +9,6 @@ public class Example01 {
 	    System.out.println("이클립스 KYH SUCCESS 444 !!!");
 	    System.out.println("소스트리 KYH SUCCESS 9999 !!!");
 	    System.out.println("이클립스 KYH SUCCESS 8888 !!!");
+	    
+	     System.out.println("소스트리 마지막  SUCCESS 777 !!!");
 	}
-}
